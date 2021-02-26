ee059c6a38a77073fd9fb8a3e7aadf0e8c9cf256
Paul Liu <oucl5976@gmail.com>
2021-02-26 18:41:12 +0000

    gh-pages

---
+++ src/components/__tests__/UseStateForm.spec.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {render, fireEvent, screen} from '@testing-library/react';
+import UseStateFrom from '../UseStateFrom';
+
+test('Input fields', () => {
+  render(<UseStateFrom />);
+  ['Name: ', 'Weight: ', 'Height: '].forEach((label) => {
+    const input = screen.getByLabelText(label);
+    expect(input.value).toBe('');
+    fireEvent.change(input, {target: {value: 'hello world'}});
+    expect(input.value).toBe('hello world');
+  });
+});
